8d350438297a75aa82bf8291f8c072a433378e4c
Prince Thakur <60568028+Princet99@users.noreply.github.com>
2025-05-31 09:00:19 +0530

    Update MyLoan.js
    Bug fix on connecting user id

--- src/component/myloan/MyLoan.js
+++ src/component/myloan/MyLoan.js
@@ -215,7 +215,7 @@ const MyLoan = () => {
 
       const auth0Sub = users.sub;
 
-      const response = await fetch("http://localhost:3030/update", {
+      const response = await fetch("${apiUrl}/update", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
