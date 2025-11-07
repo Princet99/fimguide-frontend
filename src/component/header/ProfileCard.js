d1005775b3bc0ea626f6265b5526b47512015b58
prince7hakur <pt1133557799@gmail.com>
2025-11-07 18:16:42 +0530

    log out url changed

--- src/component/header/ProfileCard.js
+++ src/component/header/ProfileCard.js
@@ -10,7 +10,7 @@ const devauthlogin = () => {
 const devauthcallback = () => {};
 
 const devauthlogout = () => {
-  window.location.href = `https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-5aa96b64-0844-4867-a3fe-d31753bdc2f6/fim-do-api-service/dev-auth-logout`;
+  window.location.href = `https://api.fimdreams.com/dev-auth-logout`;
 };
 
 const ProfileCard = () => {
