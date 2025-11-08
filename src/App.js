31fb7430d5fab5784180af02f446d85b74498ce0
prince7hakur <pt1133557799@gmail.com>
2025-11-09 01:40:26 +0530

    removed unused variable

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import Header from "./component/header/Header";
 // import Login from "./component/login/login";
 import MyLoan from "./component/myloan/MyLoan";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer, toast } from "react-toastify";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,6 +1,6 @@
 import "./Header.css";
 import ProfileCard from "./ProfileCard";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
